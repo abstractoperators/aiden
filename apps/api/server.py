f70f0b72fcc2eb980e7bc2cb7c14c5457fa0152a
Michael Deng <michaeldeng2003@gmail.com>
2025-02-05 10:14:33 -0600

    Add uvicorn as a dependency on api. Create make commands for ease of use

--- apps/api/server.py
+++ apps/api/server.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 
 app = FastAPI()
 
@@ -6,3 +6,15 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.get("/agents/{agent_id}")
+async def get_agent(agent_id: int):
+    return {"agent_id": agent_id}
+
+
+@app.get("/agents/{agent_id}/chat")
+async def get_agent_chat(agent_id: int, request: Request):
+    request_body = await request.json()
+    message = request_body["message"]
+    return {"agent_id": agent_id, "message": message}
