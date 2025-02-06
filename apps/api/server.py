14bd94182a1711cd9c6792bec337f3f62238573d
Michael Deng <michaeldeng2003@gmail.com>
2025-02-06 11:57:48 -0600

    Add test db connection to api.

--- apps/api/server.py
+++ apps/api/server.py
@@ -1,9 +1,13 @@
+import os
+
+import psycopg2
+from dotenv import load_dotenv
 from fastapi import FastAPI, Request
 
+load_dotenv("../../.env.eve")
 app = FastAPI()
 
 
-@app.get("/")
 async def root():
     return {"message": "Hello World"}
 
@@ -18,3 +22,33 @@ async def get_agent_chat(agent_id: int, request: Request):
     request_body = await request.json()
     message = request_body["message"]
     return {"agent_id": agent_id, "message": message}
+
+
+def test_db_connection() -> bool:
+    password = os.getenv("POSTGRES_DB_PASSWORD")
+    host = os.getenv("POSTGRES_DB_HOST")
+    print(password[:3])
+    print(host[:3])
+
+    conn = psycopg2.connect(
+        database="postgres",
+        user="postgres",
+        password=password,
+        host=host,
+        port=5432,
+        connect_timeout=3,
+    )
+
+    cursor = conn.cursor()
+    try:
+        cursor.execute("SELECT 1")
+    except:
+        return False
+
+    return True
+
+
+if test_db_connection():
+    print("DB Connection Successful")
+else:
+    print("DB Connection Failed")
