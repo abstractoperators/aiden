086e924dc029971dceb7b8bd353488e2c6f357be
Michael Deng <michaeldeng2003@gmail.com>
2025-02-10 09:13:03 -0600

    [wip] jwt auth

--- apps/api/src/wallet.py
+++ apps/api/src/wallet.py
@@ -1,2 +1,17 @@
+from typing import Any
+
+import jwt
+
 with open("dynamic_pub_key.pub", "r") as f:
     dynamic_pub_key = f.read().strip()
+
+
+def validate_bearer_token(token: str):
+    jwt_token: str = token
+    if token.startswith("Bearer "):
+        jwt_token = token.split(" ")[1]
+
+    decoded: dict[str, Any] = jwt.decode(
+        jwt_token, dynamic_pub_key, algorithms=["RS256"]
+    )
+    print(decoded)
