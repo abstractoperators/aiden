e5062059162de3d77d20b57c474edd74c0c5fd98
Michael Deng <michaeldeng2003@gmail.com>
2025-02-11 14:35:46 -0600

    Reduce redundancy in validate_bearer_token

--- apps/api/src/utils.py
+++ apps/api/src/utils.py
@@ -35,12 +35,4 @@ def verify_decoded_token(decoded: dict[str, Any]) -> bool:
 
 def validate_bearer_token(token: str) -> bool:
     decoded = decode_bearer_token(token)
-
-    if time.time() > decoded["exp"]:
-        return False
-
-    dynamic_environment_id = "2c6dd185-953d-411c-a7a7-dfd109b611c9"
-    if decoded["iss"] != f"app.dynamicauth.com/{dynamic_environment_id}":
-        return False
-
-    return True
+    return verify_decoded_token(decoded)
