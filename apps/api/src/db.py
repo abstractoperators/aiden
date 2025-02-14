43d40f3b108e94595c3e436644d3c3072d2c52f8
Michael Deng <michaeldeng2003@gmail.com>
2025-02-14 10:15:00 -0600

    Add functions to retrieve and update runtimes for agents

--- apps/api/src/db.py
+++ apps/api/src/db.py
@@ -34,3 +34,15 @@ def get_runtimes(cursor: Tcursor):
     cursor.execute("SELECT * from RUNTIMES")
     runtimes = cursor.fetchall()
     return runtimes
+
+
+def get_runtime_for_agent(cursor: Tcursor, agent_id: str) -> str:
+    cursor.execute("SELECT * from RUNTIMES WHERE agent_id = %s", (agent_id,))
+    runtime = cursor.fetchone()
+    return runtime[0] if runtime else ""
+
+
+def update_runtime(cursor: Tcursor, runtime_id: str, agent_id: str):
+    cursor.execute(
+        "UPDATE RUNTIMES SET agent_id = %s WHERE id = %s", (agent_id, runtime_id)
+    )
