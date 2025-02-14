cc24a692cf6002015589e592c75340ebec6ab450
Michael Deng <michaeldeng2003@gmail.com>
2025-02-14 11:13:28 -0600

    Fix update_runtime in api db. Lock requests version

--- apps/api/src/db.py
+++ apps/api/src/db.py
@@ -51,7 +51,8 @@ def get_runtime_for_agent(cursor: Tcursor, agent_id: str) -> str:
     return runtime[0] if runtime else ""
 
 
-def update_runtime(cursor: Tcursor, runtime_id: str, agent_id: str):
-    cursor.execute(
-        "UPDATE RUNTIMES SET agent_id = %s WHERE id = %s", (agent_id, runtime_id)
-    )
+def update_runtime(cursor: Tcursor, url: str, agent_id: str = ""):
+    """
+    Updates the agent_id for a runtime.
+    """
+    cursor.execute("UPDATE RUNTIMES SET agent_id = %s WHERE url = %s", (agent_id, url))
