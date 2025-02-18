c52f0cd1ab9143042741dba68430e217aa43d65b
Michael Deng <michaeldeng2003@gmail.com>
2025-02-18 10:26:44 -0600

    Add sqlmodel + sqlalchemy engine

--- apps/api/src/db.py
+++ apps/api/src/db.py
@@ -1,12 +1,47 @@
 import os
+from contextlib import contextmanager
 
 import psycopg2.pool
 from psycopg2.extensions import cursor as Tcursor
+from sqlalchemy import URL
+from sqlmodel import Session as SQLModelSession
+from sqlmodel import create_engine
 
 # TODO: ORM
 
 db_password = os.getenv("POSTGRES_DB_PASSWORD")
 db_host = os.getenv("POSTGRES_DB_HOST")
+if db_password and db_host:
+    SQLALCHEMY_DATABASE_URL = URL.create(
+        drivername="postgresql+psycopg2",
+        username="postgres",
+        password=db_password,
+        host=db_host,
+        database="postgres",
+    )
+    connect_args = {}
+else:
+    SQLALCHEMY_DATABASE_URL = URL.create(
+        drivername="sqlite",
+        username="",
+        password="",
+        host="",
+        database="test.db",
+    )
+    connect_args = {"check_same_thread": False}
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args=connect_args)
+
+
+@contextmanager
+def Session():
+    session = SQLModelSession(engine)
+    try:
+        yield session
+    finally:
+        session.close()
+
+
 pool = psycopg2.pool.SimpleConnectionPool(
     1,
     20,
