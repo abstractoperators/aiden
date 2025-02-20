d5b5f6b1e89da1b2ca8c5256d0aa5b4353a41ea3
Dance <dance@abstractoperators.ai>
2025-02-20 14:13:22 -0800

    removed placeholder agents except for kent in consumer leaderboard

--- apps/frontend/app/agents/tabs.tsx
+++ apps/frontend/app/agents/tabs.tsx
@@ -8,47 +8,16 @@ async function getLive(): Promise<Agent[]> {
   return [
     {
       name: "Kent",
-      ticker: "POS",
-      marketCapitalization: 1000000000,
-      holderCount: 9001,
-    },
-    {
-      name: "Michael",
-      ticker: "SWOLE",
-      marketCapitalization: 999999999,
-      holderCount: 69,
-    },
-    {
-      name: "Dance",
-      ticker: "DANCE",
-      marketCapitalization: 1000,
-      holderCount: 420,
+      ticker: "KENT",
+      marketCapitalization: 0,
+      holderCount: 0,
     },
   ]
 }
 
 async function getIncubating(): Promise<Agent[]> {
   // TODO: replace with API call
-  return [
-    {
-      name: "Gang",
-      ticker: "SOP",
-      marketCapitalization: 1,
-      holderCount: 3.14159,
-    },
-    {
-      name: "Deng",
-      ticker: "ELOWS",
-      marketCapitalization: 2,
-      holderCount: 42,
-    },
-    {
-      name: "Ecnad",
-      ticker: "ECNAD",
-      marketCapitalization: 3,
-      holderCount: 0,
-    },
-  ]
+  return []
 }
 
 export default async function AgentsTabs() {
