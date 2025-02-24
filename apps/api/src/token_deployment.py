9df3e10fc0af5f677b196317e9d17874e17c94fd
Michael Deng <michaeldeng2003@gmail.com>
2025-02-24 15:26:32 -0600

    Add unsigned transaction functions for buying and selling tokens

--- apps/api/src/token_deployment.py
+++ apps/api/src/token_deployment.py
@@ -1,9 +1,12 @@
-import os
 import json
-from web3 import AsyncWeb3, AsyncHTTPProvider
+import os
+
 from eth_account import Account
+from web3 import AsyncHTTPProvider, AsyncWeb3
+
 
 SEI_RPC_URL = os.getenv("SEI_RPC_URL")  # Get the SEI EVM RPC URL
+
 
 # Connects to SEI's EVM RPC and deploys a new instance of the token contract.
 async def deploy_token(name, ticker):
@@ -18,7 +21,10 @@ async def deploy_token(name, ticker):
         raise ConnectionError("Failed to connect to Sei Network")
 
     # Load ABI and bytecode
-    with open("./src/bonding_token/artifacts/contracts/BondingCurveToken.sol/BondingCurveToken.json", "r") as f:
+    with open(
+        "./src/bonding_token/artifacts/contracts/BondingCurveToken.sol/BondingCurveToken.json",
+        "r",
+    ) as f:
         contract_json = json.load(f)
     contract_abi = contract_json["abi"]
     contract_bytecode = contract_json["bytecode"]
@@ -34,13 +40,15 @@ async def deploy_token(name, ticker):
     gas_price = await w3.eth.gas_price
     chain_id = await w3.eth.chain_id
     # Build transaction with the given name and ticker
-    deploy_txn = await contract.constructor(name, ticker).build_transaction({
-        'from': deployer_address,
-        'nonce': nonce,
-        'gas': 5000000,
-        'gasPrice': gas_price,
-        'chainId': chain_id
-    })
+    deploy_txn = await contract.constructor(name, ticker).build_transaction(
+        {
+            "from": deployer_address,
+            "nonce": nonce,
+            "gas": 5000000,
+            "gasPrice": gas_price,
+            "chainId": chain_id,
+        }
+    )
 
     # Sign and send deployment transaction
     signed_txn = w3.eth.account.sign_transaction(deploy_txn, PRIVATE_KEY)
@@ -59,6 +67,7 @@ async def deploy_token(name, ticker):
 
     return contract_address
 
+
 async def buy_token(buy_amount, contract_address):
     w3 = AsyncWeb3(AsyncHTTPProvider(SEI_RPC_URL))
     # Check connection
@@ -76,13 +85,13 @@ async def buy_token(buy_amount, contract_address):
     chain_id = await w3.eth.chain_id
 
     buy_txn = {
-        'to': contract_address,
-        'value': buy_amount,
-        'from': deployer_address,
-        'nonce': nonce,
-        'gas': 300000,
-        'gasPrice': gas_price,
-        'chainId': chain_id
+        "to": contract_address,
+        "value": buy_amount,
+        "from": deployer_address,
+        "nonce": nonce,
+        "gas": 300000,
+        "gasPrice": gas_price,
+        "chainId": chain_id,
     }
 
     signed_buy_txn = w3.eth.account.sign_transaction(buy_txn, PRIVATE_KEY)
@@ -92,3 +101,71 @@ async def buy_token(buy_amount, contract_address):
     buy_receipt = await w3.eth.wait_for_transaction_receipt(buy_tx_hash)
     print(f"Tokens bought successfully in tx: {buy_receipt.transactionHash.hex()}")
     return buy_receipt
+
+
+async def sell_token_unsigned(amount, contract_abi, contract_address, user_address):
+    """
+    Returns an unsigned transaction to sell tokens.
+
+    amount: Amount of tokens to sell
+
+    contract_address: Address of the token contract
+
+    user_address: Address of the user selling the tokens
+    """
+    w3 = AsyncWeb3(AsyncHTTPProvider(SEI_RPC_URL))
+    if not await w3.is_connected():
+        raise ConnectionError("Failed to connect to Sei Network")
+
+    contract = w3.eth.contract(address=contract_address, abi=contract_abi)
+
+    nonce = await w3.eth.get_transaction_count(user_address)
+    gas_price = await w3.eth.gas_price
+    chain_id = await w3.eth.chain_id
+
+    sell_function_data = contract.encodeABI(fn_name="sellTokens", args=[amount])
+
+    unsigned_txn = {
+        "from": user_address,
+        "to": contract_address,
+        "value": 0,  # Selling tokens, not sending SEI/eth
+        "gas": 300000,
+        "gasPrice": gas_price,
+        "nonce": nonce,
+        "data": sell_function_data,
+        "chainId": chain_id,
+    }
+    return unsigned_txn
+
+
+async def buy_token_unsigned(amount, contract_address, user_address):
+    """
+    Returns an unsigned transaction to buy tokens.
+
+    amount: Amount of tokens to buy
+
+    contract_abi: ABI of the token contract
+
+    contract_address: Address of the token contract
+
+    user_address: Address of the user buying the tokens
+    """
+    w3 = AsyncWeb3(AsyncHTTPProvider(SEI_RPC_URL))
+    if not await w3.is_connected():
+        raise ConnectionError("Failed to connect to Sei Network")
+
+    nonce = await w3.eth.get_transaction_count(user_address)
+    gas_price = await w3.eth.gas_price
+    chain_id = await w3.eth.chain_id
+
+    unsigned_buy_txn = {
+        "to": contract_address,
+        "value": amount,  # Amount in SEI
+        "from": user_address,
+        "nonce": nonce,
+        "gas": 300000,
+        "gasPrice": gas_price,
+        "chainId": chain_id,
+    }
+
+    return unsigned_buy_txn
