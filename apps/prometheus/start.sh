758b9a33524fbac48bd952af0f0beb492a1b3e9c
Michael Deng <michaeldeng2003@gmail.com>
2025-02-28 10:07:36 -0600

    Add support for using different prometheus config depending on env.

---
+++ apps/prometheus/start.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# CMD entrypoint adapted to use different config files based on ENV variable
+# CMD entrypoint found here https://github.com/prometheus/prometheus/blob/main/Dockerfile
+
+ENV=${ENV:-dev}
+
+case "$ENV" in
+  prod)
+    CONFIG_FILE="/etc/prometheus/prometheus.prod.yml"
+    ;;
+  stage)
+    CONFIG_FILE="/etc/prometheus/prometheus.staging.yml"
+    ;;
+  *)
+    CONFIG_FILE="/etc/prometheus/prometheus.dev.yml"
+    ;;
+esac
+
+echo "Starting Prometheus with config file: $CONFIG_FILE"
+
+exec /bin/prometheus \
+  --config.file="$CONFIG_FILE" \
+  --storage.tsdb.path=/prometheus \
+  "$@"
