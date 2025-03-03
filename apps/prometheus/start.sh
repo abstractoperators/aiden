2faae518bd40d381db94ae5d0b753c0a3d79f8e8
Michael Deng <michaeldeng2003@gmail.com>
2025-03-03 10:50:13 -0600

    Add web config files for prometheus with basic auth.
    https://prometheus.io/docs/guides/basic-auth/

--- apps/prometheus/start.sh
+++ apps/prometheus/start.sh
@@ -4,23 +4,29 @@
 # CMD entrypoint found here https://github.com/prometheus/prometheus/blob/main/Dockerfile
 
 ENV=${ENV:-dev}
+echo "ENV: $ENV"
 
 case "$ENV" in
   prod)
     CONFIG_FILE="/etc/prometheus/prometheus.yml"
+    WEB_CONFIG_FILE="/etc/prometheus/web.yml"
     ;;
   staging)
     CONFIG_FILE="/etc/prometheus/prometheus.staging.yml"
+    WEB_CONFIG_FILE="/etc/prometheus/web.staging.yml"
     ;;
   *)
     CONFIG_FILE="/etc/prometheus/prometheus.dev.yml"
+    WEB_CONFIG_FILE="/etc/prometheus/web.dev.yml"
     ;;
 esac
 
 echo "Starting Prometheus with config file: $CONFIG_FILE"
+echo "Starting Prometheus with web config file: $WEB_CONFIG_FILE"
 
 exec /bin/prometheus \
   --config.file="$CONFIG_FILE" \
   --storage.tsdb.path=/prometheus \
   --web.listen-address="0.0.0.0:9090" \
+  --web.config.file="$WEB_CONFIG_FILE" \
   "$@"
