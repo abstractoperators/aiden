531afea323ef9de23bce47b9950b0746546ca2ce
Michael Deng <michaeldeng2003@gmail.com>
2025-03-07 09:45:02 -0600

    Add tests for GET user, token, and runtime endpoints

--- apps/api/src/test_endpoints.py
+++ apps/api/src/test_endpoints.py
@@ -1,6 +1,8 @@
-from uuid import UUID
+from uuid import UUID, uuid4
 
 from fastapi.testclient import TestClient
+
+from src.db.models import Runtime, Token, User
 from src.models import AgentPublic
 from src.server import app
 
@@ -14,6 +16,30 @@ def test_ping():
     assert response.status_code == 200
 
 
+def test_get_user():
+    # real user hardcoded to AbopDev
+    user_id: UUID = "292392de-ddfd-48a8-81fc-b61b0212c391"
+    public_key: str = "0xe98493C9943097f1127dD1C55257fbA8eD2E3211"
+    fake_user_id: UUID = uuid4()
+    fake_public_key: str = "0xfoobarbaz"
+
+    response = client.get("/users")
+    assert response.status_code == 200
+
+    response = client.get(f"/users?public_key={public_key}&user_id={user_id}")
+    assert response.status_code == 400
+
+    response = client.get(f"/users?public_key={fake_public_key}")
+    assert response.status_code == 404
+
+    response = client.get(f"/users?user_id={fake_user_id}")
+    assert response.status_code == 404
+
+    response = client.get(f"/users?public_key={public_key}")
+    assert response.status_code == 200
+    User.model_validate(response.json())
+
+
 def test_get_agents():
     response = client.get("/agents")
     assert response.status_code == 200
@@ -22,8 +48,10 @@ def test_get_agents():
             agent_public = AgentPublic.model_validate(agent)
             if agent_public.runtime_id:
                 assert agent_public.runtime
+                Runtime.model_validate(agent_public.runtime)
             if agent_public.token_id:
                 assert agent_public.token
+                Token.model_validate(agent_public.token)
         except Exception as e:
             assert False, f"{e} {agent}"
 
@@ -39,5 +67,47 @@ def test_get_agent():
             assert agent_public.runtime
         if agent_public.token_id:
             assert agent_public.token
+    except Exception as e:
+        assert False, e
+
+
+def test_get_tokens():
+    response = client.get("/tokens")
+    assert response.status_code == 200
+    for token in response.json():
+        try:
+            Token.model_validate(token)
+        except Exception as e:
+            assert False, e
+
+
+def test_get_token():
+    # Hardcoded to kentbottest $kbt
+    id: UUID = "1bdcf274-1a16-400a-bf50-94cbf5188483"
+    response = client.get(f"/tokens/{id}")
+    assert response.status_code == 200
+    try:
+        Token.model_validate(response.json())
+    except Exception as e:
+        assert False, e
+
+
+def test_get_runtimes():
+    response = client.get("/runtimes")
+    assert response.status_code == 200
+    for runtime in response.json():
+        try:
+            Runtime.model_validate(runtime)
+        except Exception as e:
+            assert False, e
+
+
+def test_get_runtime():
+    # Hardcoded to runtime 2
+    id: UUID = "c9053b67-5e7a-4f55-af83-7438dc8777e2"
+    response = client.get(f"/runtimes/{id}")
+    assert response.status_code == 200
+    try:
+        Runtime.model_validate(response.json())
     except Exception as e:
         assert False, e
