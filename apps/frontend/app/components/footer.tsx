226fc0df19b1b4f4ae23462740d4cacf8e1aa65c
Dance <dance@abstractoperators.ai>
2025-03-13 16:06:29 -0700

    set intend spacing to 2 spaces

--- apps/frontend/app/components/footer.tsx
+++ apps/frontend/app/components/footer.tsx
@@ -2,36 +2,36 @@ import Link from "next/link";
 
 const baseFooterStyle = "w-full flex justify-center";
 enum footerStyles {
-   landing = `${baseFooterStyle}`,
-   main = `${baseFooterStyle} bg-background/10 backdrop-blur supports-[backdrop-filter]:bg-background/10`,
+  landing = `${baseFooterStyle}`,
+  main = `${baseFooterStyle} bg-background/10 backdrop-blur supports-[backdrop-filter]:bg-background/10`,
 }
 
 interface variantOutputs {
-   footerStyle: footerStyles;
+  footerStyle: footerStyles;
 }
 interface variantProp {
-   variant?: "landing" | "main";
+  variant?: "landing" | "main";
 }
 function getVariantOutputs(variant: variantProp["variant"]): variantOutputs {
-   switch (variant) {
-      case "landing":
-         return {
-            footerStyle: footerStyles.landing,
-         };
-      case "main":
-      default:
-         return {
-            footerStyle: footerStyles.main,
-         };
-   }
+  switch (variant) {
+    case "landing":
+        return {
+          footerStyle: footerStyles.landing,
+        };
+    case "main":
+    default:
+        return {
+          footerStyle: footerStyles.main,
+        };
+  }
 }
 
 export default function Footer({ variant }: variantProp) {
-   const { footerStyle } = getVariantOutputs(variant);
+  const { footerStyle } = getVariantOutputs(variant);
 
-   return (
-      <footer className={footerStyle}>
-         <div className="w-full py-4 text-center text-sm text-gray-500">© 2025 AIDEN. All rights reserved.</div>
-      </footer>
-   );
+  return (
+    <footer className={footerStyle}>
+        <div className="w-full py-4 text-center text-sm text-gray-500">© 2025 AIDEN. All rights reserved.</div>
+    </footer>
+  );
 }
