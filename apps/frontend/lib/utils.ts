4950e1d253ceb5bb1a7c10fc9fb8154e57af4ea9
Dance <dance@abstractoperators.ai>
2025-03-14 20:31:33 -0700

    add snake case to camel case converter

--- apps/frontend/lib/utils.ts
+++ apps/frontend/lib/utils.ts
@@ -1,6 +1,15 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
-export function cn(...inputs: ClassValue[]) {
+function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+function snakeCaseToCamelCase(str: string) {
+  return str.replace(/([A-Z])/g, (group) => "_" + group.toLowerCase());
+}
+
+export {
+  cn,
+  snakeCaseToCamelCase,
 }
