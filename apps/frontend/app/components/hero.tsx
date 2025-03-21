6cad86e364d959258458c3eedda383586f5f9536
Dance <dance@abstractoperators.ai>
2025-03-20 21:04:59 -0700

    updated hero for better funneling, added custom colors to tailwind

--- apps/frontend/app/components/hero.tsx
+++ apps/frontend/app/components/hero.tsx
@@ -1,5 +1,7 @@
-import LightGhost from "@/public/brand_assets/light-ghost.svg"
+import Logo from "@/public/brand_assets/dark-ghost.svg"
 import Image from "next/image"
+import Link from "next/link"
+import { Button } from "./ui/button"
 
 export default function Hero() {
   return (
@@ -8,17 +10,35 @@ export default function Hero() {
         <Image
           priority={true}
           className="w-1/2"
-          src={LightGhost}
+          src={Logo}
           alt="AIDEN"
         />
-        <h1 className="text-3xl tracking-widest font-bold sm:text-4xl md:text-5xl lg:text-6xl">
+        <h1 className="text-3xl tracking-widest font-bold sm:text-4xl md:text-5xl lg:text-6xl text-neutral-800">
           AIDEN
         </h1>
       </div>
       <div className="flex flex-col space-y-4 items-center justify-center">
-        <h1 className="text-2xl tracking-wide font-bold sm:text-2xl md:text-3xl lg:text-4xl">
+        <h1 className="text-2xl tracking-wide sm:text-2xl md:text-3xl lg:text-4xl text-neutral-800">
           High Performance Onchain Agents
         </h1>
+      </div>
+      <div className="flex justify-center items-center gap-8 m-8">
+        <Button
+          className="text-xl p-8 bg-anakiwa-darker dark:bg-anakiwa-darker hover:bg-anakiwa-dark dark:hover:bg-anakiwa text-white dark:hover:text-black rounded-lg transition font-bold"
+          asChild
+        >
+          <Link href="/agents">
+            Chat with an Agent
+          </Link>
+        </Button>
+        <button
+          style={{
+            background: "radial-gradient(276.98% 284.46% at 25.31% 13.8%, rgba(213, 210, 255, .8) 0%, rgba(6, 109, 255, .8) 100%)",
+          }}
+          className="font-bold text-xl px-8 py-4 text-black rounded-lg transition hover:opacity-80"
+        >
+          Join the Waitlist
+        </button>
       </div>
     </section>
   )
