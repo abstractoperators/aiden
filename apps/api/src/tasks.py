2c88858e6fd2a45346f6a8091979bd435e816a65
Michael Deng <michaeldeng2003@gmail.com>
2025-03-24 08:39:36 -0500

    Fix typo "10min -> 5min in description"

--- apps/api/src/tasks.py
+++ apps/api/src/tasks.py
@@ -45,7 +45,7 @@ def setup_periodic_tasks(sender: Celery, **kwargs):
     sender.add_periodic_task(
         300,
         healthcheck_runtimes.s(),
-        name="Healthcheck all runtimes every 10 minutes",
+        name="Healthcheck all runtimes every 5 minutes",
     )
 
 
