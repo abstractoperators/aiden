36e4618fe9152e591bf5792649885a9b9e889bb0
Michael Deng <michaeldeng2003@gmail.com>
2025-03-27 10:36:37 -0500

    Revert "Bad attempt at getting auth token on FE"
    This reverts commit d4c2d443c067ab15d98709e156e676b890de918c.

--- apps/frontend/app/agents/page.tsx
+++ apps/frontend/app/agents/page.tsx
@@ -1,23 +1,12 @@
 import AgentsTabs from "./tabs";
-import { getAuthToken } from "@dynamic-labs/sdk-react-core";
-import { cookies } from "next/headers";
 
-export default async function AgentsDashboard() {
-  const kookooforkokopuffs = await cookies();
-  if (kookooforkokopuffs == undefined) {
-    throw new Error("No cookies found");
-  }
-  const tokenCookie = kookooforkokopuffs.get("dynamic_authentication_token");
-  if (!tokenCookie) {
-    throw new Error("Authentication token not found in cookies");
-  }
-  const token = tokenCookie.value;
+export default function AgentsDashboard() {
   return (
     <main className="flex-1 flex flex-col gap-8 m-8 sm:m-4 md:m-8 lg:m-16">
       <h1 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl">
         Agents
       </h1>
-      <AgentsTabs auth_token={token}></AgentsTabs>
+      <AgentsTabs />
     </main>
-  );
+  )
 }
--- apps/frontend/app/agents/tabs.tsx
+++ apps/frontend/app/agents/tabs.tsx
@@ -1,14 +1,11 @@
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { DataTable } from "@/components/ui/data-table";
-import { columns } from "./columns";
+import { columns } from "./columns"
 import { getIncubating, getEnlightened } from "@/lib/api/agent";
 
-type AgentsTabsProps = {
-  auth_token: string;
-};
-export default async function AgentsTabs({ auth_token }: AgentsTabsProps) {
-  const enlightened = await getEnlightened(auth_token);
-  const incubating = await getIncubating(auth_token);
+export default async function AgentsTabs() {
+  const enlightened = await getEnlightened();
+  const incubating = await getIncubating();
 
   return (
     <Tabs defaultValue="enlightened">
@@ -23,5 +20,5 @@ export default async function AgentsTabs({ auth_token }: AgentsTabsProps) {
         <DataTable columns={columns} data={incubating} />
       </TabsContent>
     </Tabs>
-  );
+  )
 }
