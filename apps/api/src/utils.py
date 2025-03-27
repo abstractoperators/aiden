03f93885025db77e1eae9e6848648c6d645cc581
Michael Deng <michaeldeng2003@gmail.com>
2025-03-27 09:05:38 -0500

    Update decode_bearer_token to use PyJWT.decode_complete to verify claims.

--- apps/api/src/utils.py
+++ apps/api/src/utils.py
@@ -1,8 +1,7 @@
 from typing import Any, TypeVar
 
-import jwt
 from fastapi import HTTPException
-from jwt import PyJWKClient
+from jwt import PyJWKClient, PyJWT
 
 from src.db.models import Base
 
@@ -13,6 +12,21 @@ def obj_or_404(obj: T | None, T) -> T:
     if not obj:
         raise HTTPException(status_code=404, detail=f"{T.__name__} Not found")
     return obj
+
+
+options = {
+    "verify_signature": True,
+    "verify_exp": True,
+    "verify_nbf": True,
+    "verify_iat": True,
+    "verify_iss": True,
+    "verify_aud": False,
+    "verify_sub": False,
+    "verify_jti": False,
+    "strict_iat": False,
+    "require": ["exp", "iat", "nbf"],
+}
+py_jwt = PyJWT(options)
 
 
 def decode_bearer_token(jwt_token: str, pyjwk_client: PyJWKClient) -> dict[str, Any]:
@@ -26,10 +40,4 @@ def decode_bearer_token(jwt_token: str, pyjwk_client: PyJWKClient) -> dict[str, 
     print(f"signing_key: {signing_key}")
     print(f"jwt_token: {jwt_token}")
 
-    decoded: dict[str, Any] = jwt.decode(
-        jwt_token,
-        signing_key,
-        algorithms=["RS256"],
-        options={"verify_signature": False},
-    )
-    return decoded
+    return py_jwt.decode_complete(jwt_token, signing_key)
