d8e7628903f0994b009bbd7376abcb63adfd6698
Dance <dance@abstractoperators.ai>
2025-04-03 04:31:27 -0700

    augment getRuntime with available runtime retrieval when no id is provided

--- apps/frontend/lib/api/runtime.ts
+++ apps/frontend/lib/api/runtime.ts
@@ -28,12 +28,30 @@ async function getRuntimes(unused: boolean = true): Promise<Runtime[]> {
 }
 
 async function getRuntime(
-  runtimeId: string,
+  runtimeId?: string,
 ): Promise<Runtime> {
-  return getResource<Runtime>({
-    baseUrl: baseUrlSegment,
-    resourceId: runtimeId,
-  })
+  if (runtimeId) {
+    return getResource<Runtime>({
+      baseUrl: baseUrlSegment,
+      resourceId: runtimeId,
+    })
+  } else {
+    const unusedRuntimes = await getRuntimes()
+    // if no unused runtime, get a random one
+    // TODO: delete once getlatestruntime is implemented on API
+    const runtime: Runtime = unusedRuntimes.length ?
+      unusedRuntimes[0] :
+      await (async () => {
+        console.log("No unused runtimes to start an agent, getting a random runtime")
+        const runtimes: Runtime[] = (
+          await getRuntimes(false)
+          .then(list => list.length ? list : Promise.all([createRuntime()]))
+        )
+        return runtimes[Math.floor(Math.random() * runtimes.length)]
+      })()
+    
+    return runtime
+  }
 }
 
 async function createRuntime(): Promise<Runtime> {
