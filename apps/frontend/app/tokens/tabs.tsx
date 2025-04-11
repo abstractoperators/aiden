b5565575ce31e8cd5558794d95280ede5d1e066e
Michael Deng <michaeldeng2003@gmail.com>
2025-04-11 14:14:59 -0500

    Fix tokens/tabs.tsx to use Result<Token> instead of Token

--- apps/frontend/app/tokens/tabs.tsx
+++ apps/frontend/app/tokens/tabs.tsx
@@ -1,11 +1,20 @@
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { DataTable } from "@/components/ui/data-table";
 import { columns } from "./columns"
-// import { getIncubating, getEnlightened } from "@/lib/api/agent";
 import { getTokens } from "@/lib/api/token";
+import { isErrorResult } from "@/lib/api/result";
 
 export default async function AgentsTabs() {
     const tokens = await getTokens();
+    if (isErrorResult(tokens)) {
+        return (
+            <main className="flex-1 self-stretch flex flex-col gap-8 m-8 bg-neutral-600/40 backdrop-blur p-8 rounded-xl">
+                <h1 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl">
+                    Unable to retrieve Token information!
+                </h1>
+            </main>
+        )
+    }
 
     return (
         <Tabs defaultValue="Tokens">
@@ -13,7 +22,7 @@ export default async function AgentsTabs() {
                 <TabsTrigger value="Tokens">Tokens</TabsTrigger>
             </TabsList>
             <TabsContent value="Tokens">
-                <DataTable columns={columns} data={tokens} />
+                <DataTable columns={columns} data={tokens.data} />
             </TabsContent>
         </Tabs>
     )
