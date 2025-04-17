92f2a2f37c1b139d8ad7de5be5a3315222eca4d2
Dance <dance@abstractoperators.ai>
2025-04-16 18:31:21 -0700

    debug fix for access list in api server

--- apps/api/src/auth.py
+++ apps/api/src/auth.py
@@ -161,7 +161,7 @@ def get_wallets_from_token(
 
 
 def access_list(
-    *required_permissions: str,
+    *required_permissions: tuple[str, ...],
 ) -> Callable:
     """
     If the JWT token is valid, returns the access list associated with the token
@@ -177,9 +177,9 @@ def access_list(
         # Access lists will be in lists field of payload
         # https://docs.dynamic.xyz/authentication-methods/auth-tokens
         payload_access_list: list[str] = payload.get("lists", [])
-        if not set(payload_access_list) == set(required_permissions):
+        if not set(required_permissions).issubset(set(payload_access_list)):
             raise HTTPException(
-                detail="User does not have the required permissions",
+                detail=f"User does not have the required permissions {required_permissions} {set(required_permissions)} in {payload_access_list} {set(payload_access_list)}",
                 status_code=403,
             )
 
