dd54085a5cfdf766e9d4ab4f17d6b4922d699541
Dance <dance@abstractoperators.ai>
2025-06-13 13:50:15 -0700

    use function type generic syntax

--- apps/api/src/utils.py
+++ apps/api/src/utils.py
@@ -1,14 +1,11 @@
-from typing import TypeVar
-
 from fastapi import HTTPException
-
 
 from src.db.models import Base
 
-T = TypeVar("T", bound=Base)
-
-
-def obj_or_404(obj: T | None, T) -> T:
+def obj_or_404[T: Base](
+    obj: T | None,
+    fallback_type: type[T],
+) -> T:
     if not obj:
-        raise HTTPException(status_code=404, detail=f"{T.__name__} Not found")
+        raise HTTPException(status_code=404, detail=f"{fallback_type.__name__} Not found")
     return obj
