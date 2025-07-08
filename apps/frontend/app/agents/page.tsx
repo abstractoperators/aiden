69b849167f5a7c0e9f73f46237cb1013f08dc8ca
crypto-cooker <mateo.kova0510@gmail.com>
2025-07-08 10:59:21 +0000

    fix: update components with prebuilt components

--- apps/frontend/app/agents/page.tsx
+++ apps/frontend/app/agents/page.tsx
@@ -2,6 +2,8 @@
 
 import { useState } from "react";
 import AgentsTabs from "./tabs";
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
 
 export default function AgentsDashboard() {
   const [searchQuery, setSearchQuery] = useState("");
@@ -10,15 +12,14 @@ export default function AgentsDashboard() {
         {/* Search Field */}
         {/* <div className="w-full max-w-5xl mb-8">
           <div className="flex items-center border bg-panel rounded-xl w-full px-2 py-1.5">
-            <input
-              type="text"
+            <Input
               placeholder="Search Agents"
               className="flex-1 bg-transparent outline-none text-white placeholder:text-gray-400
                placeholder:text-lg pl-2 pr-2"
             />
-            <button className="flex items-center justify-center w-10 h-10 bg-[#1B202F] border rounded-lg ml-2">
+            <Button className="flex items-center justify-center w-10 h-10 bg-[#1B202F] border rounded-lg ml-2">
               <svg className="w-5 h-5 text-gray-200" fill="none" stroke="currentColor" strokeWidth="2" viewBox="0 0 24 24"><circle cx="11" cy="11" r="8"/><line x1="21" y1="21" x2="16.65" y2="16.65"/></svg>
-            </button>
+            </Button>
           </div>
         </div> */}
         {/* Table Container with Tabs */}
