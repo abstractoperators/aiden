056d22c436d86383ba7fcd7cc5d6840d913f540c
Dance <dance@abstractoperators.ai>
2025-07-10 17:30:55 -0700

    fix spacing on creation page

--- apps/frontend/app/user/agents/creation/page.tsx
+++ apps/frontend/app/user/agents/creation/page.tsx
@@ -35,17 +35,17 @@ export default async function AgentCreation() {
   return (
     <div className="flex flex-col items-center m-16">
       <div className="w-full max-w-5xl flex flex-col gap-6 items-center">
-        <h1 className="text-2xl text-foreground font-alexandria sm:text-xl md:text-2xl lg:text-3xl mb-8">
+        <h1 className="text-2xl text-foreground font-sans sm:text-xl md:text-2xl lg:text-3xl mb-8">
           Create an Agent
         </h1>
         {
           isSuccessResult(userAgents) ?
           (
             userAgents.data.length > 0 && !session.user.scopes.includes('admin') ?
-            <h2 className="text-foreground">You may only have one agent at a time!</h2> :
+            <h2 className="text-foreground font-sans">You may only have one agent at a time!</h2> :
             <FormTabs />
           ) :
-          <h2 className="text-foreground">Unable to prepare agent creation form!</h2>
+          <h2 className="text-foreground font-sans">Unable to prepare agent creation form!</h2>
         }
       </div>
     </div>
